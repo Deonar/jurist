e1dc8fc260e4a4d4d91ac01b7e6c4a9ce953e865
deonarius <deonarius@gmail.com>
2018-03-07 23:11:21 +0200

    Contact page

--- app/js/common.js
+++ app/js/common.js
@@ -37,8 +37,13 @@ $(function() {
     //Readmore
     $('.news-description').readmore({
         collapsedHeight: 75,
+        speed: 500,
         moreLink: '<a class="readmore-js-toggle" href="#">Подробнее</a>',
-        lessLink: '<a class="readmore-js-toggle" href="#">Свернуть</a>'
+        lessLink: '<a class="readmore-js-toggle" href="#">Свернуть</a>',
+        afterToggle: function(trigger, element, expanded) {
+            if(! expanded) {
+                $('html, body').animate( { scrollTop: element.offset().top }, {duration: 100 } );
+            }
+        }
     });
-
 });
